ec946fa1fe4c5d85cc6dc565ce6be11efbc35e87
angeltried <angeltried@icloud.com>
2022-11-30 23:29:46 -0600

    first commit tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <main>
+    <section className="bg-blue-700 min-h-screen">
+      <h1>hello</h1>
+    </section>
+  </main>
+  );
+}
+
+export default App;

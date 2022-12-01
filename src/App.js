d5dafdfa16753ad49748e08aa18ede5adc85e30f
angeltried <angeltried@icloud.com>
2022-12-01 00:48:59 -0600

    added some body

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import {BsFillMoonStarsFill} from "react-icons/bs";
-import './App.css';
+import {AiFillLinkedin, AiFillGithub, AiFillInstagram } from "react-icons/ai";
+
+
 
 function App() {
   return (
   <main className="bg-white-500 px-10">
     <section className="min-h-screen">
       <nav className="py-10 mb-12 flex justify-between">
-        <h1 className="text-xl font-mono">Angel Gutierrez Suarez</h1>
+        <h1 className="text-xl font-mono">Sleepnosis</h1>
         <ul className="flex items-center">
           <li><BsFillMoonStarsFill className="cursor-pointer text-2xl"/></li>
           <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
         </ul>
       </nav>
+      <div className="text-center p-10">
+        <h2 className="text-5xl py-2 text-teal-600 font-medium">Angel Gutierrez Suarez</h2>
+        <h3 className="text-2xl py-2">Developer and designer</h3>
+        <p className="text-md py-5 leading-8 text-gray-800">Freelancer providing services for web development.</p>
+      </div>
+      <div className="text-5xl flex justify-center gap-16 py-2 text-gray-600">
+        <AiFillLinkedin/>
+        <AiFillGithub/>
+        <AiFillInstagram/>
+      </div>
+      <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-2xl w-60 h-70 mt-10">
+        <img src="me.png"></img>
+      </div>
     </section>
   </main>
   );
